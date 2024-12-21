92ce2e5b8d2bc2ac3cb626f59e511a67022e8eee
Kostiantyn Kolosov <rayleefx@protonmail.com>
2024-12-21 23:30:21 +0200

    Initiated

--- Sources/TMSwipeActions/TMSwipeActions.swift
+++ Sources/TMSwipeActions/TMSwipeActions.swift
@@ -1,2 +1,11 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+import OSLog
+
+public class HelloWorld {
+    func printHelloWorld() {
+        Logger(.default).info("Hello, World!")
+    }
+}
